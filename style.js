60660f310d39ae86e6f58bea797e1269bcaa8622
Jewel mia <you@example.com>
2024-02-12 23:22:57 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,37 @@
+/**
+ * objective : get base , height of a triangle. ca
+ * 
+ * 
+ */
+
+//Triangle
+
+function calculateTriangleArea(){
+    const triangleBase = document.getElementById('triangle-base');
+    const triangleBaseJewel = triangleBase.value;
+    const base = parseFloat(triangleBaseJewel)
+    console.log(base);
+
+
+
+    const triangleHeight = document.getElementById('triangle-height');
+    const triangleInput = triangleHeight.value;
+    const height = parseFloat(triangleInput);
+    console.log(height);
+
+//calculate
+    const area = 0.5 * base * height;
+    console.log('This is the result', area);
+
+    //display triangle area 
+
+    const triangleArea =  document.getElementById('triangle-area');
+    triangleArea.innerText = area;
+
+} 
+
+
+
+
+ 
+
